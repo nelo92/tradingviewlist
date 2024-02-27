6d76e9cab72f956ff7377321e3b14a9c159c2038
nelo92 <manuel.augusto.92@gmail.com>
2024-02-27 22:57:44 +0100

    Init createlist

---
+++ src/app/constants/firebase.constants.ts
@@ -0,0 +1,5 @@
+// collection
+export const COLLECTION_LISTS = 'lists';
+
+// field for collection lists
+export const FIELD_SYMBOL = 'symbol';
--- src/app/createlist/createlist.component.ts
+++ src/app/createlist/createlist.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { Lists } from '../models/firebase.models';
+import * as CONSTANTS from '../constants';
+
+
 
 @Component({
   selector: 'app-createlist',
@@ -10,14 +15,20 @@ export class CreatelistComponent implements OnInit {
   value = 'Symbol';
   //  symbolFormControl = new FormControl('', [Validators.required, Validators.email]);
 
+  lists$: Observable<Lists[]> | undefined;
+
   constructor(private store: AngularFirestore) {}
 
   ngOnInit(): void {
-    console.log('init');
+    console.log('init...');
+
+    this.lists$ = this.store.collection(CONSTANTS.COLLECTION_LISTS).valueChanges({ id: 'id' }) as Observable<Lists[]>;
+    console.log(this.lists$);
+
   }
 
   onCreate() {
-    // window.alert('create');
-    // console.log('create');
+    console.log('onCreate...');
+    this.store.collection(CONSTANTS.COLLECTION_LISTS).add({symbol: this.value});
   }
 }
